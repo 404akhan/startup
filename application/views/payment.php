f64115875abe97c989718066bff5b0a4e4891b53
gordon <manchingchan1992@yahoo.com.hk>
2016-09-20 12:50:46 +0800

    [Payment] show chart

--- application/views/payment.php
+++ application/views/payment.php
@@ -15,7 +15,7 @@
         <h3 class="orange_text" style="text-align:center;"><span id="amount_result">0</span> ( <span id="amount_month">0</span> HKD/mo )</h3>
       </div>
       <div class="row col-sm-12" id="sandbox-container">
-      <input type="text" class="form-control"/>
+      <input type="text" class="form-control" value="09/20/2016" />
       <script>
       $('#sandbox-container input').datepicker({
         orientation: "bottom auto"
@@ -27,10 +27,30 @@
 
 
      <div class="row">
-      <img style="width: 100%;" src="<?php echo base_url(); ?>assets/fintech/group-6@2x.png"/>
-    </div>
-    <div class="row">
-      <img style="width: 100%;" src="<?php echo base_url(); ?>assets/fintech/image-5@2x.png"/>
+      <script>
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
+
+                function drawChart() {
+                   var data = google.visualization.arrayToDataTable([
+                     ['Month', 'HKD'],
+                     ['May',  120],
+                     ['Jun',  260],
+                     ['Jul',  320],
+                     ['Aug',  540]
+                     ]);
+
+                   var options = {
+                     title: '',
+                     hAxis: {title: '2016',  titleTextStyle: {color: '#333'}},
+                     vAxis: {minValue: 0}
+                 };
+
+                 var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+                 chart.draw(data, options);
+             }
+         </script>
+         <div id="chart_div" style="width: 100%; height: 100%;"></div>
     </div>
     <div class="row col-sm-12">
       <div class="col-xs-9">
@@ -39,6 +59,7 @@
       <div class="col-xs-3">
         <select class="form-control">
           <option>By Month</option>
+          <option>
         </select>
       </div>
     </div>
