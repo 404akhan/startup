6295d441d84a5ff64f33e08332961b1c4d7bf311
gordon <manchingchan1992@yahoo.com.hk>
2016-09-20 11:11:22 +0800

    [Startup] index page fixed style

--- application/views/startup.php
+++ application/views/startup.php
@@ -2,59 +2,65 @@
 <div class="st-main">
     <div class="container">
         <div class="row" style="margin: 15px 0px;">
-            <div class="col-xs-2">
-                <img src="<?php echo base_url(); ?>assets/fintech/logo.png" alt="Start Up">
-            </div>
 
-            <div class="col-xs-6">
-                <b><?php echo $name; ?></b><br>
-                <p><?php echo $amount_invested; ?> - amount invested</p>
-            </div>
 
-            <div class="col-xs-2 col-xs-offset-2">
-                <p>ROI: <?php echo $persentage_expecting; ?>%</p>
-            </div>
-        </div>
+          <table style="width:100%;">
+             <tr>
+                <td style="width: 10%;"><img src="<?php echo base_url(); ?>assets/fintech/logo.png">
 
-        <div class="row">
-            <img height="300px" width="100%" src="<?php echo base_url(); ?>assets/fintech/image-1@2x.png" alt="">
-        </div>
+              </td>
+              <td><?php echo $name; ?><br/>
+                  <?php echo $amount_invested; ?> - amount invested</td>
+                  <td style="text-align:right;">ROI:<?php echo $persentage_expecting; ?>%</td>
 
-        <div class="row">
-            <a href="<?php echo base_url(); ?>index.php/payment/index/1" class="btn btn-success btn-lg" style="float: right;">
-                Invest
-            </a>
-        </div>
+              </tr>
+          </table>
 
-        <div class="row">
-            <div class="col-md-12">
-                <p>The social enterprise to fund and train 16-21 year old selected orphans in China, funded through an educational program teaching English and Chinese through online media.</p>
-            </div>
-        </div>
 
-        <div class="st-choices">
-            <ul class="nav nav-tabs">
-                <li class="active"><a data-toggle="tab" href="#home">Performance</a></li>
-                <li><a data-toggle="tab" href="#menu1">Projection</a></li>
-                <li><a data-toggle="tab" href="#menu2">Marketplace</a></li>
-            </ul>
 
-            <div class="tab-content">
-                <div id="home" class="tab-pane fade in active">
-                    <img width="100%" src="<?php echo base_url(); ?>assets/fintech/group-6@2x.png" alt="">
-                    <img width="100%" src="<?php echo base_url(); ?>assets/fintech/image-5@2x.png" alt="">
-                </div>
-                <div id="menu1" class="tab-pane fade">
-                    <img width="100%" src="<?php echo base_url(); ?>assets/fintech/group-6@2x.png" alt="">
-                    <img width="100%" src="<?php echo base_url(); ?>assets/fintech/image-5@2x.png" alt="">
-                </div>
-                <div id="menu2" class="tab-pane fade">
 
-                </div>
-            </div>
-        </div>
 
+      </div>
+
+      <div class="row">
+        <img height="300px" width="100%" src="<?php echo base_url(); ?>assets/fintech/image-1@2x.png" alt="">
     </div>
+
+    <div class="row">
+        <a href="<?php echo base_url(); ?>index.php/payment/index/1" class="btn btn-success btn-lg" style="float: right;">
+            Invest
+        </a>
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <p>Our vision lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa, itaque.</p>
+        </div>
+    </div>
+
+    <div class="st-choices">
+        <ul class="nav nav-tabs">
+            <li class="active"><a data-toggle="tab" href="#home">Performance</a></li>
+            <li><a data-toggle="tab" href="#menu1">Projection</a></li>
+            <li><a data-toggle="tab" href="#menu2">Marketplace</a></li>
+        </ul>
+
+        <div class="tab-content">
+            <div id="home" class="tab-pane fade in active">
+                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/group-6@2x.png" alt="">
+                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/image-5@2x.png" alt="">
+            </div>
+            <div id="menu1" class="tab-pane fade">
+                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/group-6@2x.png" alt="">
+                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/image-5@2x.png" alt="">
+            </div>
+            <div id="menu2" class="tab-pane fade">
+
+            </div>
+        </div>
+    </div>
+
+</div>
 </div>
 
 <div class="st-main-bottom">
@@ -74,8 +80,8 @@
                             <img height="60px" width="60px" src="<?php echo base_url(); ?>assets/fintech/head1.jpg" alt="">
                         </td>
                         <td>
-                            <h4>Owen Chang</h4>
-                            <p>CEO, ex Banker, now full time philanthropist dedicated to raising the future generation leaders in society.</p>
+                            <h4>Three Line Item</h4>
+                            <p>Secondary line lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, ratione.</p>
                         </td>
                     </tr>
                     <tr>
@@ -83,8 +89,8 @@
                             <img height="60px" width="60px" src="<?php echo base_url(); ?>assets/fintech/head2.jpg" alt="">
                         </td>
                         <td>
-                            <h4>Ashish Kramer</h4>
-                            <p>COO, accomplished entrepreneur, manages business daily functions.</p>
+                            <h4>Three Line Item</h4>
+                            <p>Secondary line lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, ratione.</p>
                         </td>
                     </tr>
                     <tr>
@@ -92,8 +98,8 @@
                             <img height="60px" width="60px" src="<?php echo base_url(); ?>assets/fintech/head3.jpg" alt="">
                         </td>
                         <td>
-                            <h4>Aydana Zholdasova</h4>
-                            <p>Children coordinator, with a heart of gold, manages orphanage and educational program</p>
+                            <h4>Three Line Item</h4>
+                            <p>Secondary line lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, ratione.</p>
                         </td>
                     </tr>
                 </tbody>
@@ -103,24 +109,24 @@
         <div class="row">
             <table class="table">
                 <thead>
-                <tr>
-                    <th colspan="2">
-                        <h3 style="text-align: center;">Contact Info</h3>
-                    </th>
-                </tr>
+                    <tr>
+                        <th colspan="2">
+                            <h3 style="text-align: center;">Contact Info</h3>
+                        </th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>
-                        <img src="<?php echo base_url(); ?>assets/fintech/image-6@2x.png" alt="Start Up">
-                    </td>
-                    <td>
-                        <p>http://fthackathon.com/</p>
-                        <p>852-1234 5678</p>
-                        <p>hk@finnovasia.com</p>
+                    <tr>
+                        <td>
+                            <img src="<?php echo base_url(); ?>assets/fintech/image-6@2x.png" alt="Start Up">
+                        </td>
+                        <td>
+                            <p>http://fthackathon.com/</p>
+                            <p>852-1234 5678</p>
+                            <p>hk@finnovasia.com</p>
 
-                    </td>
-                </tr>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
@@ -128,20 +134,20 @@
         <div class="row">
             <table class="table">
                 <thead>
-                <tr>
-                    <th colspan="2">
-                        <h3 style="text-align: center;">Downloads</h3>
-                    </th>
-                </tr>
+                    <tr>
+                        <th colspan="2">
+                            <h3 style="text-align: center;">Downloads</h3>
+                        </th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>
-                        <p>Report_2016_Q3</p>
-                        <p>Report_2016_Q2</p>
-                        <p>Report_2016_Q1</p>
-                    </td>
-                </tr>
+                    <tr>
+                        <td>
+                            <p>Report_2016_Q3</p>
+                            <p>Report_2016_Q2</p>
+                            <p>Report_2016_Q1</p>
+                        </td>
+                    </tr>
                 </tbody>
             </table>
         </div>
