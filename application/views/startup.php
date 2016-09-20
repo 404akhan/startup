b75aef8b1b4cca7c20f019f55affe84059e96c5b
gordon <manchingchan1992@yahoo.com.hk>
2016-09-20 12:02:19 +0800

    add chart

--- application/views/startup.php
+++ application/views/startup.php
@@ -5,26 +5,32 @@
 
 
           <table style="width:100%;">
-             <tr>
-                <td style="width: 10%;"><img src="<?php echo base_url(); ?>assets/fintech/logo.png">
+           <tr>
+            <td style="width: 10%;"><img src="<?php echo base_url(); ?>assets/fintech/logo.png">
 
-              </td>
-              <td><?php echo $name; ?><br/>
-                  <?php echo $amount_invested; ?> - amount invested</td>
-                  <td style="text-align:right;">Yield:<?php echo $persentage_expecting; ?>%</td>
+            </td>
+            <td><?php echo $name; ?><br/>
+              <?php echo $amount_invested; ?> - amount invested</td>
+              <td style="text-align:right;">Yield:<?php echo $persentage_expecting; ?>%</td>
 
-              </tr>
-          </table>
-
-
+          </tr>
+      </table>
 
 
 
-      </div>
 
-      <div class="row">
-        <img height="300px" width="100%" src="<?php echo base_url(); ?>assets/fintech/blabla.png" alt="">
-    </div>
+
+  </div>
+
+  <div class="row">
+    <img height="300px" width="100%" src="<?php echo base_url(); ?>assets/fintech/image-1@2x.png" alt="">
+</div>
+
+<div class="row">
+    <a href="<?php echo base_url(); ?>index.php/payment/index/1" class="btn btn-success btn-lg" style="float: right;">
+        Invest
+    </a>
+</div>
 
     <div class="row">
         <a href="<?php echo base_url(); ?>index.php/payment/index/1" class="btn btn-success btn-lg" style="float: right;">
@@ -32,33 +38,50 @@
         </a>
     </div>
 
-    <div class="row">
-        <div class="col-md-12">
-            <p>The social enterprise to fund and train 16-21 year old selected orphans in China, funded through an educational program teaching English and Chinese through online media.</p>
-        </div>
-    </div>
 
-    <div class="st-choices">
-        <ul class="nav nav-tabs">
-            <li class="active"><a data-toggle="tab" href="#home">Performance</a></li>
-            <li><a data-toggle="tab" href="#menu1">Projection</a></li>
-            <li><a data-toggle="tab" href="#menu2">Marketplace</a></li>
-        </ul>
+<div class="st-choices">
+    <ul class="nav nav-tabs">
+        <li class="active"><a data-toggle="tab" href="#home">Performance</a></li>
+        <li><a data-toggle="tab" href="#menu1">Projection</a></li>
+        <li><a data-toggle="tab" href="#menu2">Marketplace</a></li>
+    </ul>
 
-        <div class="tab-content">
-            <div id="home" class="tab-pane fade in active">
-                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/group-6@2x.png" alt="">
-                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/image-5@2x.png" alt="">
-            </div>
-            <div id="menu1" class="tab-pane fade">
-                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/group-6@2x.png" alt="">
-                <img width="100%" src="<?php echo base_url(); ?>assets/fintech/image-5@2x.png" alt="">
-            </div>
-            <div id="menu2" class="tab-pane fade">
+    
+    <div class="tab-content">
+        <div id="home" class="tab-pane fade in active">
+            <script>
+                google.charts.load('current', {'packages':['corechart']});
+                google.charts.setOnLoadCallback(drawChart);
 
-            </div>
-        </div>
-    </div>
+                function drawChart() {
+                   var data = google.visualization.arrayToDataTable([
+                     ['Month', 'HKD'],
+                     ['May',  120],
+                     ['Jun',  260],
+                     ['Jul',  320],
+                     ['Aug',  540]
+                     ]);
+
+                   var options = {
+                     title: '',
+                     hAxis: {title: '2016',  titleTextStyle: {color: '#333'}},
+                     vAxis: {minValue: 0}
+                 };
+
+                 var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+                 chart.draw(data, options);
+             }
+         </script>
+         <div id="chart_div" style="width: 100%; height: 100%;"></div>
+     </div>
+     <div id="menu1" class="tab-pane fade">
+         
+     </div>
+     <div id="menu2" class="tab-pane fade">
+
+     </div>
+ </div>
+</div>
 
 </div>
 </div>
@@ -166,21 +189,21 @@
                         <td>
                             <div class="row">
                                 <table style="width:100%;">
-                                     <tr>
-                                        <td style="width: 10%;"><img src="<?php echo base_url(); ?>assets/fintech/logo.png">
+                                   <tr>
+                                    <td style="width: 10%;"><img src="<?php echo base_url(); ?>assets/fintech/logo.png">
 
-                                      </td>
-                                      <td>XXX<br/>
-                                          1014K - investors</td>
-                                          <td style="text-align:right;">Yield: 10%</td>
+                                    </td>
+                                    <td>XXX<br/>
+                                      1014K - investors</td>
+                                      <td style="text-align:right;">Yield: 10%</td>
 
-                                      </tr>
-                                  </table>
-                            </div>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+                                  </tr>
+                              </table>
+                          </div>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+  </div>
 </div>
